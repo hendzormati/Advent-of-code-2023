e5935b8f38f46a7a25564a0217c4a00a3a9f8b3b
hendzormati <101046285+hendzormati@users.noreply.github.com>
2023-12-08 00:25:25 +0100

    Update day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -1,3 +1,4 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,14 +10,29 @@ for(int i=0;i<q;i++)
 {
     string s;
     cin>>s;
-    int l=0,r=0;
+    string num[9]={ "one", "two", "three", "four", "five", "six", "seven", "eight","nine"};
+        int l=0,r=0;
     for(int j=0;j<s.size();j++)
     {
-       if(isdigit(s[j]))
+        //part 1
+        if(isdigit(s[j]))
         {
-            if(l==0){l=(s[j]-'0')*10;r=(s[j]-'0');}
-            else r=(s[j]-'0');
+            if(l==0)
+                l=(s[j]-'0')*10;
+            r=s[j]-'0';
         }
+       else
+        {  //part 2 
+            for(int k=0;k<9;k++)
+                if(s.find(num[k],j)==j)
+                    {
+                    if(l==0)
+                        l=(k+1)*10;
+                    r=k+1;
+                    j+=num[k].size()-2;// case eightwo 
+                    break;
+                    }
+       }
     }
     sum+=l+r;
 }
